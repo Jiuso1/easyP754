385285832213a252649550bb99d601a875ce6ff5
Jiuso <jesusgg200x@gmail.com>
2025-02-01 15:29:21 +0100

    RichTextFX added.

--- src/main/java/com/team/MainApplication.java
+++ src/main/java/com/team/MainApplication.java
@@ -4,11 +4,11 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.fxmisc.richtext.StyleClassedTextArea;
 
 public class MainApplication extends Application {
     public void start(Stage primaryStage) {
@@ -16,12 +16,12 @@ public class MainApplication extends Application {
         HBox hBox = new HBox();//Create a horizontal box layout.
         Button button = new Button("Calculate");//Create a button with specified text.
         TextField textField = new TextField();//Create a TextField.
-        TextArea textArea = new TextArea("CO\u2082\n" +
-                                        "NO\u2082\n");
+        StyleClassedTextArea output = new StyleClassedTextArea();
+        output.setEditable(false);
         hBox.setAlignment(Pos.TOP_CENTER);//The horizontal box is aligned with the top center.
-        hBox.getChildren().addAll(textField, button);//Add textfield and button as children of hBox.
-        root.getChildren().addAll(hBox, textArea);//Add hBox and textArea as children of root.
-        Scene scene = new Scene(root, 1000, 600);//Create a scene specifying the hBox and the size.
+        hBox.getChildren().addAll(textField, button);//Add textField and button as children of hBox.
+        root.getChildren().addAll(hBox, output);//Add hBox and output as children of root.
+        Scene scene = new Scene(root, 1000, 600);//Create a scene specifying root and window size.
         primaryStage.setScene(scene);//Add scene to the stage.
         primaryStage.show();//Make the stage visible.
     }
